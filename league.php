7c391a16f67072dbf770b38b8b013803431105fc
Marcel van der Meer <61979125+VanderMeerM@users.noreply.github.com>
2026-01-25 22:20:28 +0100

    Add files via upload

--- league.php
+++ league.php
@@ -373,6 +373,10 @@ if (sizeof($matchesInRound) > 3) {
   <div id="arrow_up">↑</div>';
 };
 
+
+if ($_GET['id']) {
+include ('./lineup.php');
+}
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
