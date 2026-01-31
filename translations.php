0be7e00b84aa7a6213004625323b90cfc22cbe66
Marcel van der Meer <61979125+VanderMeerM@users.noreply.github.com>
2026-01-31 17:17:07 +0100

    Add files via upload

--- translations.php
+++ translations.php
@@ -78,12 +78,3 @@ $array_rounds = array(
 'Final' => 'finale'
 
 );
-
-$array_position_player {
-  'Midfielder' => 'Middenvelder'
-}
-
-$array_countries {
-  'Netherlands' => 'Nederland',
-  'Spain' => 'Spanje'
-}
