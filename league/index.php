6cf1aaa41f608433c4a248a54876e389d4aa3a22
VanderMeerM <marcelmeer@gmail.com>
2026-05-22 21:17:48 +0200

    edited penalty

--- league/index.php
+++ league/index.php
@@ -90,7 +90,6 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => 'GET',
   CURLOPT_HTTPHEADER => array(
       'x-rapidapi-key: ' . $api_key .'',
-     //'x-rapidapi-key: 863bcd048478f98225b64bced629b376',
      'x-rapidapi-host: v3.football.api-sports.io',
     
   ),
--- penalty/index.php
+++ penalty/index.php
@@ -14,18 +14,47 @@
     <title>Penalties schieten</title>
 </head>
 
-
-<body class="text-center">
-
 <?php 
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://www.apicountries.com/countries");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$output = curl_exec($ch);
-curl_close($ch);
-//echo $output;
-?>
+$curl_url = "https://www.apicountries.com/countries";
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $curl_url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+  ),
+));
+
+$response = curl_exec($curl);
+
+$response = json_decode($response, true);
+
+//print_r($response[0]['translations']['nl']);
+
+$array_countries_nl = [];
+
+for ($i=0; $i < sizeof($response); $i++) {
+
+  array_push($array_countries_nl, $response[$i]['translations']['nl']);
+
+  }
+
+sort($array_countries_nl);
+
+//print_r($array_countries_nl);
+
+
+echo '
+
+<body class="text-center">
 
     <h2>Selecteer land of voer (club)naam in </h2>
 
@@ -35,14 +64,20 @@ curl_close($ch);
        <p>
 
     <select id="countryA">
-            <option selected >Selecteer land:</option>
-    </select>
-   <p></p>
-    <input placeholder='Naam club' id='clubA'>
+        <option selected >Selecteer land:</option>';
 
+
+        for ($i=0; $i < sizeof($array_countries_nl); $i++) {
+            echo '<option>'. $array_countries_nl[$i] .'</option>';
+        }
+
+echo '
+</select>
+   <p></p>
+    <input placeholder="Naam club" id="clubA">
     <p>
 
-</div> 
+</div>
 
 <div class="float-lg-right">
        <strong> Team B </strong> 
@@ -50,10 +85,17 @@ curl_close($ch);
        <p>
 
     <select id="countryB">
-        <option selected>Selecteer land:</option>
+        <option selected>Selecteer land:</option>';
+
+       
+        for ($i=0; $i < sizeof($array_countries_nl); $i++) {
+            echo '<option>'. $array_countries_nl[$i] .'</option>';
+        }
+
+echo '
 </select>
 <p></p>
-<input placeholder='Naam club' id='clubB'>
+<input placeholder="Naam club" id="clubB">
 
 <p></p>
 
@@ -91,7 +133,10 @@ curl_close($ch);
         <div id="playerB"></div>
 </div>
    
-<div id="winner"> </div>
+<div id="winner"> </div>';
+
+?>
+
 
 </body>
 
